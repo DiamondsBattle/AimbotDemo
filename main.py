03e7e7a58d4e7a699897b0aead72d342f950fa54
Louis Jarraud <diamondsbattle@tutanota.com>
2021-06-29 11:14:22 +0200

    Fixed offset when target went to high

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ def getAngleDif(f, t):
     # The camera has a top offset to the body, so we are using camera_pivot
     # to get the camera's position and not the body's
     h_dif = arctan((f.z - t.z) / (f.x - t.x))
-    f_y = f.camera_pivot.y - f.y
-    v_dif = arctan((t.y - f_y + 2) / sqrt((t.z - f.z)**2 + (t.x - f.x)**2))
+    f_y = f.camera_pivot.y + f.y + 3
+    v_dif = arctan((t.y - f_y + 3) / sqrt((t.z - f.z)**2 + (t.x - f.x)**2))
 
     if t.x - f.x < 0:
-        return 360 - radToDeg(h_dif) - 90, radToDeg(v_dif)
+        return 360 - radToDeg(h_dif) - 90, radToDeg(-v_dif)
     elif t.x - f.x > 0:
-        return 360 - radToDeg(h_dif) + 90, radToDeg(v_dif)
+        return 360 - radToDeg(h_dif) + 90, radToDeg(-v_dif)
 
 
 def update():
@@ -28,9 +28,11 @@ app = Ursina()
 
 window.vsync = True
 
-ground = Entity(model='cube', texture='white_cube', scale=Vec3(100, 1, 100), position=Vec3(2, -2, 2), collider='box', texture_scale=(2, 2, 1))
+ground = Entity(model='cube', color=color.rgba(0, 0, 0, .2), texture='white_cube', scale=Vec3(100, 1, 100), position=Vec3(2, -2, 2), collider='box', texture_scale=(2, 2, 1))
 
 enemy = Entity(model='cube', color=color.red, texture='white_cube', position=Vec3(2, 1, 2))
+enemy.animate_position(Vec3(7, 4, 2), duration=3)
+enemy.animate_position(Vec3(-7, -4, -2), duration=3, delay=3)
 me = FirstPersonController(position=Vec3(4, 2, -4))
 # QWERTY to AZERTY rebinds
 input_handler.rebind('z', 'w')
